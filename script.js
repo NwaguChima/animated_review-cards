0d40c4548a0b2e5e3edf36d1fe4b48f0d96343a2
NwaguChima <nwaguchima48@gmail.com>
2022-07-22 21:10:59 +0100

    calculated total and average reviews

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const reviewBarsContainer = document.querySelector(".review-bars");
+const reviewRowsContainer = document.querySelector(".review-rows");
+
+const REVIEWS = {
+  5: 120,
+  4: 40,
+  3: 20,
+  2: 0,
+  1: 0,
+};
+
+const totalReviews = Object.values(REVIEWS).reduce(
+  (sum, value) => sum + value,
+  0
+);
+
+const averageReview =
+  Object.entries(REVIEWS).reduce((sum, [value, quantity]) => {
+    return sum + value * quantity;
+  }, 0) / totalReviews;
